7c8b0b490500b07865cd451d9a01d2596ddeec05
EchoStorm <RagingStorm77236@gmail.com>
2025-07-05 13:45:28 -0700

    Added chat

--- script.js
+++ script.js
@@ -11,12 +11,25 @@ function openchat() {
 
         const sentences = [
             "The quick brown fox jumps over the lazy dog.",
-            "*silent moment of cringe*",
+            "*Silent moment of cringe*",
             "To be or not to be.",
             "All that glitters is not gold. -Random person, 2018.",
             "It's 2025 and you don't know how to code?",
             "If there are 26 sheep and 10 goats on a ship, how old is the ship's captain?",
-            "Never gonna give you up, never gonna let you down."
+            "Never gonna give you up, never gonna let you down.",
+            "9+10 = ?",
+            "2+2=5",
+            "If you drop a sponge on the floor, is the floor clean or is the sponge dirty?",
+            "Sorry I got too lazy to code this message :)",
+            "Dont press those buttons.",
+            "I call it coding. (ryhmes with grug)",
+            "https://music.youtube.com/watch?v=LQ4w9xiHkrY",
+            "Fire in the hole :)",
+            "Why did you go to the website?",
+            "How was the fall?",
+            "What sound does a wallet make?",
+            "Go play neal.fun, it is a fun website.",
+            "Butter Dawg, Dawg wit da butter onem."
         ];
 
         // Function to get a random sentence
